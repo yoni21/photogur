837cb9b85277ccc17597ee1a6bad05a4600a892f
yonas samuel <yonastlolo@yahoo.com>
2017-01-27 14:35:38 -0500

    Configuring root as pictures#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  root 'pictures#index'
   get 'pictures' => 'pictures#index'
 
   post 'pictures' => 'pictures#create' # this is a new line of code
   get 'pictures/new' => 'pictures#new' # this is also a new line of code
- 
+
   get 'pictures/:id' => 'pictures#show', as: 'picture'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
