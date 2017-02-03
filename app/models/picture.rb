77575e2d255c64b6b7b66640c1df9f975026afbe
yonas samuel <yonastlolo@yahoo.com>
2017-02-02 22:27:29 -0500

    validation

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,2 +1,8 @@
 class Picture < ApplicationRecord
+
+  validates :artist, presence: true
+  validates :title, length: {in: 3..20}
+  validates :url, presence: true, uniqueness: true
+  validate_format_of :url, :with => /(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/\S*)?/i
+
 end
